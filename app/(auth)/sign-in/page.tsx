64e2427b2c4f8cb33296f1fc9783fd6e95afd9ab
Abhijeet <abhijeet2005shinde@gmail.com>
2024-12-20 21:40:01 +0530

    commit

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,12 @@
+import AuthForm from '@/components/ui/AuthForm'
 import React from 'react'
 
 const SingIn = () => {
     return (
-        <div>
-            SingIn
-        </div>
+        <section className='flex-center size-full max-sm:px-6'>
+            <AuthForm type="sign-in"
+            />
+        </section>
     )
 }
 
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,7 +14,7 @@ import { cn } from "@/lib/utils"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-// import Footer from "./Footer"
+import Footer from "./Footer"
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname();
@@ -43,7 +43,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     </Link>
                     <div className="mobilenav-sheet">
                         <SheetClose asChild>
-                            <nav className="flex h-full flex-col gap-5 pt-16 text-white">
+                            <nav className="flex h-full flex-col gap-6 pt-16 text-white">
                                 {sidebarLinks.map((item) => {
                                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
 
@@ -73,7 +73,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                             </nav>
                         </SheetClose>
 
-                        {/* <Footer user={user} type="mobile" /> */}
+                        <Footer user={user} type="mobile" />
                     </div>
                 </SheetContent>
             </Sheet>
